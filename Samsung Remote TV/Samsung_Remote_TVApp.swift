60819d651847dbe47ff2b966d9ccbec5104c3ea3
Muhammad Riefky Hadid <riefky.hd@gmail.com>
2026-03-19 20:10:34 +0700

    Initial Commit

---
+++ Samsung Remote TV/Samsung_Remote_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Samsung_Remote_TVApp.swift
+//  Samsung Remote TV
+//
+//  Created by Administrator on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Samsung_Remote_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
